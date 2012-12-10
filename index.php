7552c20962e37c1153c3f5ef43263279767728e8
Martin M2gar <maxar.hol@gmail.com>
2012-12-10 20:51:04 +0200

    dabase connection test

--- index.php
+++ index.php
@@ -7,7 +7,24 @@
       <a href="/votingCommitteeMember.php">Voting committee member</a>
     </p>
     <p>
-      <a href="/voter.php">Voter</a>
+      <a href="/voter.php">Voter</a> <br>
+<?php
+
+        $host = "hektor3.ttu.ee";
+        $user = "t073819";
+        $pass = "Ab123c";
+        $db = "t073819";
+        $con = pg_connect("host=$host dbname=$db user=$user password=$pass");
+
+        $query="Select * from valija";
+        $rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
+        while ($row = pg_fetch_row($rs)) {
+            echo "$row[0] $row[1] $row[2]<br>";
+        }
+
+        pg_close($con);
+
+?>
     </p>
   </body>
 </html>
