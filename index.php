b5207a6151a7f1e1b841782ee07ca2a0be06e127
Jaan Sepp <ja.sepp@hotmail.com>
2012-12-10 19:39:53 +0200

    Landing page first sketch

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Select role</title>
+  </head>
+  <body>
+    <p>
+      <a href="/votingCommitteeMember.php">Voting committee member</a>
+    </p>
+    <p>
+      <a href="/voter.php">Voter</a>
+    </p>
+  </body>
+</html>
