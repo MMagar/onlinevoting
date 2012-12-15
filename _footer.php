fe5d18ff2f3ee0fe370671cb5012dacac896739a
Jaan Sepp <ja.sepp@hotmail.com>
2012-12-15 12:45:22 +0200

    header separated

--- _footer.php
+++ _footer.php
@@ -1,6 +1,6 @@
-<!-- FOOTER -->
-<footer>
-  <p class="pull-right"><a href="#">Back to top</a></p>
-
-  <p>&copy; 2012 Martin Mägar, Jaan Sepp, Roel Rahkema. </p>
-</footer>
+    <footer>
+      <p class="pull-right"><a href="#">Back to top</a></p>
+      <p>&copy; 2012 Martin Mägar, Jaan Sepp, Roel Rahkema. </p>
+    </footer>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,55 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Online voting</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <link href="css/bootstrap.css" rel="stylesheet">
-  <link href="css/bootstrap-responsive.css" rel="stylesheet">
-  <link href="css/bootstrap-example.css" rel="stylesheet">
-  <link href="css/online-voting-design.css" rel="stylesheet">
-
-
-  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-  <!--[if lt IE 9]>
-  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
-
-  <!-- Fav and touch icons -->
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-  <link rel="shortcut icon" href="ico/favicon.png">
-</head>
-
-<body>
-
 <?php
-include '_nav-bar.php';
+include '_header.php';
 ?>
-
 <div id="myCarousel" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="img/examples/slide-01.jpg" alt="">
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="img/examples/slide-01.jpg" alt="">
 
-      <div class="container">
-        <div class="carousel-caption">
-          <h1>Online voting.</h1>
-          <p class="lead">Reliable democracy for lazy citizens.</p>
-          <a class="btn btn-large btn-primary" href="#">Vote now</a>
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Online voting.</h1>
+                    <p class="lead">Reliable democracy for lazy citizens.</p>
+                    <a class="btn btn-large btn-primary" href="#">Vote now</a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>
-<!-- /.carousel -->
-
-
 <div class="container marketing">
   <div class="featurette">
     <img class="featurette-image pull-right" src="img/examples/estonia1.jpg">
@@ -68,10 +34,6 @@ include '_nav-bar.php';
     include '_footer.php';
   ?>
 </div>
-
-<!-- Le javascript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap-transition.js"></script>
 <script src="js/bootstrap-alert.js"></script>
@@ -85,5 +47,3 @@ include '_nav-bar.php';
 <script src="js/bootstrap-collapse.js"></script>
 <script src="js/bootstrap-carousel.js"></script>
 <script src="js/bootstrap-typeahead.js"></script>
-</body>
-</html>
