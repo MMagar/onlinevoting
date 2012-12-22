b6e2dced9fa19a71310f09f09afe2325845d30fa
Martin M2gar <maxar.hol@gmail.com>
2012-12-22 21:34:37 +0200

    Disabled popup hide because they are removed with each request anyway.

--- js/messagePopups.js
+++ js/messagePopups.js
@@ -1,4 +1,6 @@
 var lastAlertId = 0;
+var hideMessageAfterDelay = false;
+var hideDelay = 5000;
 function messageSuccess(message) {
   messageGeneral("Success!", message, "success");
 }
@@ -13,7 +15,9 @@ function messageGeneral(header, message, type) {
   var thisID = lastAlertId;
   $('#alerts').append('<div id="alert' + thisID + '" class="alert alert-' + type + '" style="display:none;"><a type="button" class="close" href="#" onClick="$(\'#alert' + thisID + '\').hide(\'slow\');return false;">&times;</a><strong>' + header + '</strong> ' + message + '</div>');
   $('#alert' + thisID).show("slow");
-  setTimeout(function () {
-    $('#alert' + thisID).hide('slow');
-  }, 5000);
+  if(hideMessageAfterDelay) {
+    esetTimeout(function () {
+      $('#alert' + thisID).hide('slow');
+    }, hideDelay);
+  }
 }
